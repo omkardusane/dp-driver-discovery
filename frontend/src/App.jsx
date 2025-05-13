5105d8e4ad19bb2eafe5790c5e0b00b726a867ae
omkardusane <omkardusane@gmail.com>
2025-05-13 19:16:20 +0200

    client side navigation fixed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,10 @@
 import './App.css'
 import AppRoutes from './routes/AppRoutes'
+import { useNavigate } from 'react-router-dom';
 
 function App() {
 
+  const navigate = useNavigate();
   return (
     <>
       <div className="h-24 mt-1 w-full bg-gray-200 flex flex-col p-4 rounded  mx-auto">
@@ -12,17 +14,16 @@ function App() {
         <div className="w-3/20 bg-gray-100 p-4 font-mono">
           <h1 className="text-2xl font-bold mb-4 text-gray-300">Menu</h1>
           <ul className=' text-gray-400 '>
-            <li> <a href="/"> Home </a>  </li>
-            <li> <a href="/applicants"> Applicants </a> </li>
-            <li> <a href="/"> Jobs </a> </li>
-            <li> <a href="/"> Contacts </a> </li>
-
+            <li className='hover:cursor-pointer' onClick={() => navigate("/")}> Home </li>
+            <li className='hover:cursor-pointer' onClick={() => navigate("/applicants")}> Applicants </li>
+            <li className='hover:cursor-pointer' onClick={() => navigate("/")}> Jobs </li>
+            <li className='hover:cursor-pointer' onClick={() => navigate("/applicants")}> Contacts </li>
           </ul>
         </div>
         <div className='min-h-80 mt-4 pb-8 bg-gray-50 rounded w-16/20 rounded mx-auto'>
           <AppRoutes />
         </div>
-      </div>
+      </div >
 
     </ >
   )
--- frontend/src/pages/LandingPage/index.jsx
+++ frontend/src/pages/LandingPage/index.jsx
@@ -1,16 +1,16 @@
-export default () => <div>
-    <p className="text-gray-500 text-bold pt-6 mb-4 ml-8 mr-8 ">
-        Welcome to Driver Discovery!
-        <br />
-        Here you will be able to filters the drivers connected to The Company Applicants API.
-    </p>
-    <div className='items-center justify-center flex'>
-        <button className='w-md text-white font-bold py-2 px-4 rounded transition delay-50 duration-200 ease-in-out  hover:scale-110  bg-blue-400 hover:bg-blue-600 '
-            onClick={() => {
-                window.location.href = "/applicants"
-            }}
-        >
-            Enter the portal now!
-        </button>
-    </div>
-</div >
+import { useNavigate } from 'react-router-dom';
+
+export default () => {
+    const navigate = useNavigate();
+    return <div>
+        <p className="text-gray-500 text-bold pt-6 mb-4 ml-8 mr-8 ">
+            Welcome to Driver Discovery!
+            <br />
+            Here you will be able to filters the drivers connected to The Company Applicants API.
+        </p>
+        <div className='items-center justify-center flex'>
+            <button className='w-md text-white font-bold py-2 px-4 rounded transition delay-50 duration-200 ease-in-out  hover:scale-110  bg-blue-400 hover:bg-blue-600 '
+                onClick={() => navigate("/applicants")}> Enter the portal now! </button>
+        </div>
+    </div >
+}
